6c897d69df0ba7c272be5e78586914092df4ba0f
Ayomide Bajo <oluwashinabajo@gmail.com>
2022-11-18 19:25:55 +0100

    implement websocket client connection for any duration

--- audio-playground/src/main.rs
+++ audio-playground/src/main.rs
@@ -6,10 +6,24 @@
 //! Uses a delay of `LATENCY_MS` milliseconds in case the default input and output streams are not
 //! precisely synchronised.
 
+use regex::Regex;
+use std::future::Future;
+use std::time;
+// use std::time::Duration;
+use futures::sink::SinkExt;
+use futures::stream::StreamExt;
+
+use tokio::net::TcpStream;
+use tokio::time::{sleep, Duration};
+
+use tokio_tungstenite::client_async;
+use tokio_tungstenite::tungstenite::{Error as WsError, Message};
+
 // use clap::Parser;
+use anyhow::{self, Ok};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use ringbuf::HeapRb;
-use anyhow;
+use std::result::Result::Ok as Okr;
 
 #[derive(Debug)]
 // #[command(version, about = "CPAL feedback example", long_about = None)]
@@ -41,24 +55,26 @@ struct Opt {
     jack: bool,
 }
 
-fn main() -> anyhow::Result<()> {
+fn parse_duration(duration: &str) -> time::Duration {
+    let re = Regex::new(r"((?P<hour>\d+)h)?((?P<minute>\d+)m)?((?P<second>\d+)s)?").unwrap();
+    let caps = re.captures(duration).unwrap();
+    let h: u64 = caps.name("hour").map_or(0, |m| m.as_str().parse().unwrap());
+    let m: u64 = caps
+        .name("minute")
+        .map_or(0, |m| m.as_str().parse().unwrap());
+    let s: u64 = caps
+        .name("second")
+        .map_or(0, |m| m.as_str().parse().unwrap());
+    time::Duration::new(3600 * h + 60 * m + s, 0)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt {
         input_device: String::from("MacBook Pro Microphone"),
         output_device: String::from("MacBook Pro Speakers"),
-        latency: 150.0
+        latency: 150.0,
     };
-
-
-        let addr = "127.0.0.1:9000";
-    let url = format!("ws://{}", addr);
-
-    let stream = TcpStream::connect(addr).await?;
-
-    println!("Connected to {:?}", addr);
-
-    let (mut ws_stream, _) = client_async(&url, stream).await?;
-
-    println!("Handshake successful.");
 
     // Conditionally compile with jack if the feature is specified.
     #[cfg(all(
@@ -95,97 +111,163 @@ fn main() -> anyhow::Result<()> {
     let host = cpal::default_host();
 
     // Find devices.
-    let input_device = if opt.input_device == "default" {
-        host.default_input_device()
-    } else {
-        host.input_devices()?
-            .find(|x| x.name().map(|y| y == opt.input_device).unwrap_or(false))
+    // let input_device = if opt.input_device == "default" {
+    //     host.default_input_device()
+    // } else {
+    //     host.input_devices()?
+    //         .find(|x| x.name().map(|y| y == opt.input_device).unwrap_or(false))
+    // }
+
+    let addr = "127.0.0.1:9000";
+    let url = format!("ws://{}", addr);
+
+    let stream = TcpStream::connect(addr).await?;
+
+    println!("Just checking");
+    println!("Connected to {:?}", addr);
+
+    let (mut ws_stream, _) = client_async(&url, stream).await?;
+
+    println!("Handshake successful.");
+
+    let deadline = parse_duration("1m");
+    let start = time::Instant::now();
+
+    loop {
+        let elapsed = start.elapsed();
+        if deadline < elapsed {
+            break;
+        }
+        let text = String::from("Ebgudu");
+        let msg = Message::Text(text.clone());
+
+        ws_stream.send(msg).await?;
+
+        println!("Message sent: {:?}", text);
+
+        if let Some(item) = ws_stream.next().await {
+            match item {
+                Okr(msg) => {
+                    match msg {
+                        Message::Text(text) => {
+                            println!("Received text message: {}", text);
+                        }
+                        Message::Close(frame) => {
+                            println!("Received close message: {:?}", frame);
+
+                            if let Err(e) = ws_stream.close(None).await {
+                                match e {
+                                    WsError::ConnectionClosed => (),
+                                    _ => {
+                                        println!("Error while closing: {}", e);
+                                        break;
+                                    }
+                                }
+                            }
+
+                            // println!("Sent close message.");
+
+                            println!("Closing...");
+                            return Ok(());
+                        }
+                        _ => (),
+                    }
+                }
+                Err(e) => {
+                    println!("Error receiving message: \n{0:?}\n{0}", e);
+                }
+            }
+        } else {
+            break;
+        }
+
+        sleep(Duration::from_secs(1)).await;
     }
-    .expect("failed to find input device");
 
-    let output_device = if opt.output_device == "default" {
-        host.default_output_device()
-    } else {
-        host.output_devices()?
-            .find(|x| x.name().map(|y| y == opt.output_device).unwrap_or(false))
-    }
-    .expect("failed to find output device");
+    ws_stream.close(None).await?;
+    // let output_device = if opt.output_device == "default" {
+    //     host.default_output_device()
+    // } else {
+    //     host.output_devices()?
+    //         .find(|x| x.name().map(|y| y == opt.output_device).unwrap_or(false))
+    // }
+    // .expect("failed to find output device");
 
-    println!("Using input device: \"{}\"", input_device.name()?);
-    println!("Using output device: \"{}\"", output_device.name()?);
+    // println!("Using input device: \"{}\"", input_device.name()?);
+    // println!("Using output device: \"{}\"", output_device.name()?);
 
-    // We'll try and use the same configuration between streams to keep it simple.
-    let config: cpal::StreamConfig = input_device.default_input_config()?.into();
+    // // We'll try and use the same configuration between streams to keep it simple.
+    // let config: cpal::StreamConfig = input_device.default_input_config()?.into();
 
-    // Create a delay in case the input and output devices aren't synced.
-    let latency_frames = (opt.latency / 1_000.0) * config.sample_rate.0 as f32;
-    let latency_samples = latency_frames as usize * config.channels as usize;
+    // // Create a delay in case the input and output devices aren't synced.
+    // let latency_frames = (opt.latency / 1_000.0) * config.sample_rate.0 as f32;
+    // let latency_samples = latency_frames as usize * config.channels as usize;
 
     // The buffer to share samples
-    let ring = HeapRb::<f32>::new(latency_samples * 2);
-    let (mut producer, mut consumer) = ring.split();
+    // let ring = HeapRb::<f32>::new(latency_samples * 2);
+    // let (mut producer, mut consumer) = ring.split();
 
-    // Fill the samples with 0.0 equal to the length of the delay.
-    for _ in 0..latency_samples {
-        // The ring buffer has twice as much space as necessary to add latency here,
-        // so this should never fail
-        producer.push(0.0).unwrap();
-    }
+    // // Fill the samples with 0.0 equal to the length of the delay.
+    // for _ in 0..latency_samples {
+    //     // The ring buffer has twice as much space as necessary to add latency here,
+    //     // so this should never fail
+    //     producer.push(0.0).unwrap();
+    // }
 
-    let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
-        let mut output_fell_behind = false;
-        for &sample in data {
-            if producer.push(sample).is_err() {
-                output_fell_behind = true;
-            }
-        }
-        if output_fell_behind {
-            eprintln!("output stream fell behind: try increasing latency");
-        }
-    };
+    // let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+    //     let mut output_fell_behind = false;
+    //     for &sample in data {
+    //         if producer.push(sample).is_err() {
+    //             output_fell_behind = true;
+    //         }
+    //     }
+    //     if output_fell_behind {
+    //         eprintln!("output stream fell behind: try increasing latency");
+    //     }
+    // };
 
-    let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-        let mut input_fell_behind = false;
-        for sample in data {
-            *sample = match consumer.pop() {
-                Some(s) => s,
-                None => {
-                    input_fell_behind = true;
-                    0.0
-                }
-            };
-        }
-        if input_fell_behind {
-            eprintln!("input stream fell behind: try increasing latency");
-        }
-    };
+    // let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+    //     let mut input_fell_behind = false;
+    //     for sample in data {
+    //         *sample = match consumer.pop() {
+    //             Some(s) => s,
+    //             None => {
+    //                 input_fell_behind = true;
+    //                 0.0
+    //             }
+    //         };
+    //     }
+    //     if input_fell_behind {
+    //         eprintln!("input stream fell behind: try increasing latency");
+    //     }
+    // };
 
-    // Build streams.
-    println!(
-        "Attempting to build both streams with f32 samples and `{:?}`.",
-        config
-    );
-    let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn)?;
-    let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn)?;
-    println!("Successfully built streams.");
+    // // Build streams.
+    // println!(
+    //     "Attempting to build both streams with f32 samples and `{:?}`.",
+    //     config
+    // );
+    // let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn)?;
+    // let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn)?;
+    // println!("Successfully built streams.");
 
-    // Play the streams.
-    println!(
-        "Starting the input and output streams with `{}` milliseconds of latency.",
-        opt.latency
-    );
-    input_stream.play()?;
-    output_stream.play()?;
+    // // Play the streams.
+    // println!(
+    //     "Starting the input and output streams with `{}` milliseconds of latency.",
+    //     opt.latency
+    // );
+    // input_stream.play()?;
+    // output_stream.play()?;
 
-    // Run for 3 seconds before closing.
-    println!("Playing for 3 seconds... ");
-    std::thread::sleep(std::time::Duration::from_secs(5));
-    drop(input_stream);
-    drop(output_stream);
-    println!("Done!");
+    // // Run for 3 seconds before closing.
+    // println!("Playing for 3 seconds... ");
+    // std::thread::sleep(std::time::Duration::from_secs(5));
+    // drop(input_stream);
+    // drop(output_stream);
+    // println!("Done!");
     Ok(())
 }
 
-fn err_fn(err: cpal::StreamError) {
-    eprintln!("an error occurred on stream: {}", err);
-}
+// fn err_fn(err: cpal::StreamError) {
+//     eprintln!("an error occurred on stream: {}", err);
+// }
