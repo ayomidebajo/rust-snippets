4d8db2b288ea00b8e5a95bd7f554d4078b1dd35f
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-05-06 15:53:53 +0100

    add workspace

--- directory_program/src/foo.rs
+++ directory_program/src/foo.rs
@@ -3,4 +3,5 @@ pub use self::bar::Bar;
 
 pub fn do_foo() {
 	println!("Hi from foo!");
+	// println!("I'm calling bar::hello() for you: {:?}", Bar::hello());
 }
--- directory_program/src/foo/bar.rs
+++ directory_program/src/foo/bar.rs
@@ -3,5 +3,6 @@ pub struct Bar;
 impl Bar {
 	pub fn hello() {
 		println!("Hello from Bar!");
+		// "Hello from Bar!".to_string()
 	}
 }
