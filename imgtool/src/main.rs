07c6141d5426ef947e2f5cbf0ed22065952b473b
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-05-06 16:06:32 +0100

    add imgtool

--- imgtool/src/main.rs
+++ imgtool/src/main.rs
@@ -1,3 +1,9 @@
+use std::{env, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let image_path = env::args().skip(1).next().expect("no image path given");
+    let path = Path::new(&image_path);
+    let img = image::open(path).unwrap();
+       let rotated = img.rotate90();
+       rotated.save(path).unwrap();
 }
