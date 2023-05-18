978050efabee20b0cc0366f9d4b41d66e50388bf
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-05-18 14:40:50 +0100

    add first logic for task manager

--- task_manager/src/main.rs
+++ task_manager/src/main.rs
@@ -1,3 +1,31 @@
+struct TaskManager (Vec<Task>);
+
+struct Task {
+    description: String,
+    completed: bool,
+}
+
+impl Completed for Task {
+    fn completed(&self) -> bool {
+        self.completed
+    }
+
+    fn set_status(&mut self, completed: bool) {
+        self.completed = completed;
+    }
+
+    fn toggle_status(&mut self) {
+        self.completed = !self.completed
+    }
+}
+
+trait Completed {
+    fn completed(&self) -> bool;
+    fn set_status(&mut self, completed: bool);
+    fn toggle_status(&mut self);
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
